94c665c19098a715c9b3ffb4457cbdab03fb62f3
bellboys027 <bellysaputra027@gmail.com>
2025-04-02 18:51:29 +0800

    Add files via upload

---
+++ codelab/Codelab1M3.java
@@ -0,0 +1,78 @@
+// Superclass KarakterGame
+class KarakterGame {
+    private String nama;
+    private int kesehatan;
+
+    public KarakterGame(String nama, int kesehatan) {
+        this.nama = nama;
+        this.kesehatan = kesehatan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public void serang(KarakterGame target) {
+        // Akan di-override oleh subclass
+    }
+}
+
+// Subclass Pahlawan
+class Pahlawan extends KarakterGame {
+    public Pahlawan(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan Orbital Strike!");
+        target.setKesehatan(target.getKesehatan() - 20);
+        System.out.println("Kesehatan " + target.getNama() + " sekarang: " + target.getKesehatan());
+    }
+}
+
+// Subclass Musuh
+class Musuh extends KarakterGame {
+    public Musuh(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan Snake Bite!");
+        target.setKesehatan(target.getKesehatan() - 15);
+        System.out.println("Kesehatan " + target.getNama() + " sekarang: " + target.getKesehatan());
+    }
+}
+
+// Kelas utama Main
+public class Codelab1M3 {
+    public static void main(String[] args) {
+        KarakterGame karakterUmum = new KarakterGame("Karakter Umum", 100);
+        Pahlawan brimstone = new Pahlawan("Brimstone", 150);
+        Musuh viper = new Musuh("Viper", 200);
+
+        // Menampilkan status awal
+        System.out.println("Kesehatan awal: ");
+        System.out.println(brimstone.getNama() + " : " + brimstone.getKesehatan());
+        System.out.println(viper.getNama() + " : " + viper.getKesehatan());
+        System.out.println();
+
+        // Simulasi pertarungan
+        brimstone.serang(viper);
+        System.out.println();
+        viper.serang(brimstone);
+    }
+}
